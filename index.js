5170a72109fc11e97bb6819d0d784fdd3d8e6ed2
Michael Nakonechny <michaelnako@icloud.com>
2022-08-19 15:01:09 +0200

    rick and morty api

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-import toggleApiButton from './js/_apibutton.js';
+// import toggleApiButton from './js/_apibutton.js';
 
+console.clear();
+
+const apiUrl = 'https://rickandmortyapi.com/api/character';
+
+fetch(apiUrl)
+  .then(response => response.json())
+
+  .then(data => {
+    createCard(data.results);
+    console.log(data);
+  });
--- js/_apibutton.js
+++ js/_apibutton.js
@@ -1,14 +1,14 @@
 function toggleApiButton() {
-  const apis = document.querySelectorAll("[data-js="api"]");
-  const button = api.querySelector('[data-js="buttons"]')
+  const apis = document.querySelectorAll('[data-js=api]');
+  const button = api.querySelector('[data-js="buttons"]');
 
   apis.forEach(api => {
-        const button = api.querySelector('[data-js="button"]');
-        const link = api.querySelector('[data-js='link']');
-        const image = api.querySelector('[data-js="image"]')
-            button.addEventListener('click',() => {
-           link.classList.toggle("hidden")
-           image.classList.toggle('hidden')
-    })
-})
+    const button = api.querySelector('[data-js="buttons"]');
+    const link = api.querySelector('[data-js="link"]');
+    const image = api.querySelector('[data-js="image"]');
+    button.addEventListener('click', () => {
+      link.classList.toggle('hidden');
+      image.classList.toggle('hidden');
+    });
+  });
 }
