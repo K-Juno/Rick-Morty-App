01beca69a2e9d6693add56f6da68e5e1288edf18
Juno <juno1803@gmail.com>
2022-08-20 13:31:49 +0200

    add character data

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
-
+// const { Linter } = require("eslint");
 
 // import toggleApiButton from './js/_apibutton.js';
 console.clear();
 const apiUrl = "https://rickandmortyapi.com/api/character";
 fetch(apiUrl)
   .then(response => response.json())
-  .then( data => { createCards(data.results);
-    console.log(data);
+  .then(data => {
+    createCards(data.results);
+    // console.log(data);
   });
-  function createCards(person) {
-   const main = document.querySelector('[data-js=main]');
-    const ulist = document.createElement('ul');
-    const h2 = document.createElement('h2');
-    h2.textContent = "Morty";
-    main.append(h2);
+
+function createCards(person) {
+
+  const main = document.querySelector('[data-js=main]');
+  console.log(person);
+  const h2 = document.createElement('h2');
+  h2.textContent = "Characters";
+  main.append(h2);
+
+  const image = document.createElement('img');
+  main.append(image);
+  image.classList.add('img');
+  image.src = "https://rickandmortyapi.com/api/character/avatar/1.jpeg";
+
+  const section = document.createElement('section');
+  section.classList.add('section');
+  main.append(section);
+
+  const uList = document.createElement('ul');
+  section.append(uList);
+
+  person.forEach(data => {
+    const item = document.createElement('li');
     main.classList.add('list');
-    main.append(ulist);
+    uList.append(item);
+
+    const characterData = [
+      `name : ${data.name}`,
+      `gender : ${data.gender}`,
+      `species : ${data.species}`,
+      `img : ${data.image}`,
+    ];
+
+    item.textContent = characterData;
+
+    // console.log(characterData);
     // const li = document.createElement('li');
     // ulist.append(li);
-    const image = document.createElement('img');
-    main.append(image);
-    image.src = "https://rickandmortyapi.com/api/character/avatar/1.jpeg";
-    person.forEach( Skills => {
-    const item = document.createElement('li')
-    const characterSkills = [
-        `Name : ${Skills.name}`,
-        `gender : ${Skills.gender}`,
-        `species : ${Skills.species}`,
-        `img : ${Skills.image}`,
-      ];
-     ulist.append(item);
-    console.log(characterSkills)
-    characterSkills.forEach(character => {
-    })
-})};
-      //console.log(data);
+    // characterData.forEach(character => {
+  });
+};
 
-
-
-  
+// function createItem
