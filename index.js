92188ee401de7977aba9b41e7e6da4ac346690b0
Barbara Balen <barbarabalen@barbaras-air.localdomain>
2022-08-20 11:56:18 +0200

    add listand styling

--- index.js
+++ index.js
@@ -2,32 +2,38 @@
 
 // import toggleApiButton from './js/_apibutton.js';
 console.clear();
-
-const apiUrl = 'https://rickandmortyapi.com/api/character';
-
+const apiUrl = "https://rickandmortyapi.com/api/character";
 fetch(apiUrl)
   .then(response => response.json())
-
-   .then(data => {
-    createCard(data.results);
+  .then( data => { createCards(data.results);
     console.log(data);
   });
-
-  const createCard = () => {
-const main = document.querySelector('[data-js=main]');
-const section = document.createElement('section');
-const header = document.createElement('h2');
-//header.textContent = results.name;
-const image = document.createElement('img');
-//image.src = results.image;
-section.style.backgroundColor="hotpink";
-section.textContent = 'morty'
-main.append(section);
-  }
-
-  const characterDetails =  (() => {
-    
-  })
+  function createCards(person) {
+   const main = document.querySelector('[data-js=main]');
+    const ulist = document.createElement('ul');
+    const h2 = document.createElement('h2');
+    h2.textContent = "Morty";
+    main.append(h2);
+    main.classList.add('list');
+    main.append(ulist);
+    // const li = document.createElement('li');
+    // ulist.append(li);
+    const image = document.createElement('img');
+    main.append(image);
+    image.src = "https://rickandmortyapi.com/api/character/avatar/1.jpeg";
+    person.forEach( Skills => {
+    const item = document.createElement('li')
+    const characterSkills = [
+        `Name : ${Skills.name}`,
+        `gender : ${Skills.gender}`,
+        `species : ${Skills.species}`,
+        `img : ${Skills.image}`,
+      ];
+     ulist.append(item);
+    console.log(characterSkills)
+    characterSkills.forEach(character => {
+    })
+})};
       //console.log(data);
 
 
